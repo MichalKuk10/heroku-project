5a26a19e2211c3df8be815c3be5e4d4a67b6059e
michal <kuk.michal3@gmail.com>
2020-10-17 13:00:14 +0200

    initial commit

---
+++ src/main/java/com/example/myprojectheroku/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.myprojectheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Hello Heroku!");
+	}
+
+}
