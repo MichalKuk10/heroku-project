fde4d244824054391c89038d7b97eced4d5e148e
michal <kuk.michal3@gmail.com>
2020-10-17 15:06:06 +0200

    add DAO

---
+++ src/main/java/com/example/myprojectheroku/dao/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.myprojectheroku.dao;
+
+import com.example.myprojectheroku.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {}
